92e5f12a05d1847e1bec058a587302fc7fc209ac
HiddenKaiser <45469342+HiddenKaiser@users.noreply.github.com>
2023-09-28 12:12:06 -0400

    Add text encoder and decoder

--- main.py
+++ main.py
@@ -41,4 +41,37 @@ print("\n{")
 char_table = read_excel()
 print("}\n")
 
-print(char_table["a"])
+# convert text to binary
+
+def text_to_binary(text):
+    binary = ""
+    i = 0
+    while i < (len(text)):
+        key = text[i]
+        while True:
+            if i+1 < len(text) and (key + text[i+1]) in char_table:
+                i += 1
+                key = key + text[i]
+            else:
+                break
+        binary += char_table[key]
+        i += 1
+    return binary
+
+binaryEncoded = text_to_binary("the quick brown fox jumps over the lazy dog.")
+print(binaryEncoded)
+
+# shorts are 5 bits long, longs are 7 bits long, convert binary to text
+def binary_to_text(binary):
+    text = ""
+    i = 0
+    while i < len(binary):
+        if binary[i] == '0':
+            text += list(char_table.keys())[list(char_table.values()).index(binary[i:i+5])]
+            i += 5
+        else:
+            text += list(char_table.keys())[list(char_table.values()).index(binary[i:i+7])]
+            i += 7
+    return text
+
+print(binary_to_text(binaryEncoded))
